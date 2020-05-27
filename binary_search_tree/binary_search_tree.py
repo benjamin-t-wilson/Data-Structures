c989d3e7924c8a39bff2cb0ace0b57b108f7c378
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-05-26 19:10:18 -0700

    finished bst pt 1 minus for_each like Tim said

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,16 +19,76 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # create a BSTNode to use later
+        new_node = BSTNode(value)
+
+        # do our first split, we are storing smaller values to the left
+        if self.value > value:
+
+            # we have to potential conditions on either leg
+            # 1. There is a value here
+            #   - If a value is present, run the insert method of that node
+            #   to continue the process recursively
+            if self.left:
+                return self.left.insert(value)
+
+            # 2. If we don't have a value, we can go ahead and just
+            # set it to the node we have created
+            else:
+                self.left = new_node
+
+        # This else will follow the right side and reflect
+        else:
+            if self.right:
+                return self.right.insert(value)
+            else:
+                self.right = new_node
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # if the target is the current value
+        # we are done looking :D
+        if target == self.value:
+            return True
+
+        # we know we are storing lesser values left,
+        # if the target is lower than the value, we know
+        # we need to go left
+        elif target < self.value:
+
+            # if a node is present on the left side
+            # recursively return the contains method on that node
+            if self.left:
+                return self.left.contains(target)
+
+            # if we have reached the end of the left tail
+            # it does not exist in here
+            else:
+                return False
+
+        # assuming target is higher, move right and mirror
+        else:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # since we know all higher values go right,
+        # this is going to be really easy :D
+
+        # if we have a node to the right
+        # recursively return this method
+        # continuing down that leg
+        if self.right:
+            return self.right.get_max()
+
+        # assuming we have hit the end of the tree
+        # on the right leg, we are at the max value
+        else:
+            return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
